20093d9b5545fcede7a696f11ca33af7a192cd6a
MubbashirHussain <mubbashirhhussain@gmail.com>
2025-08-02 00:32:44 +0500

    Add Prisma integration and update dependencies
    - Added Prisma and @prisma/client to package.json and package-lock.json.
    - Updated .gitignore to exclude generated Prisma files.
    - Imported PrismaModule in app.module.ts for database management.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './config/supabase/supabase.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './config/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     SupabaseModule, 
-    AuthModule
+    AuthModule, 
+    PrismaModule,
   ],
 })
 export class AppModule {}
