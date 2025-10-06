da6c5cb9dcd865ea4d9b214ea73ee22aaa06e545
mubbashirhussain <mubbashirhhussain@gmail.com>
2025-10-06 22:00:52 +0500

    feat: implement volunteer registration functionality with controller, service, and routes

---
+++ src/controllers/volunteer.controller.ts
@@ -0,0 +1,16 @@
+import response from "../utils/response.js";
+import type { Request, Response } from "express";
+import type { functionReturnObjectType } from "../types/index.js";
+import * as VolunteerService from "../services/volunteer.service.js";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const result: functionReturnObjectType = await VolunteerService.register(req);
+    return response.basicControllerRes(res, result);
+  } catch (error) {
+    console.log(error);
+    return response.resInternalError(res, error);
+  }
+};
+
+
